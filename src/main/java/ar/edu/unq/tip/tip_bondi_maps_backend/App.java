5bf81567a8440c4a84a3e9bff67dc1a9565b17a5
toledoJavier <toledojavier90@gmail.com>
2016-08-26 16:26:34 -0300

    init commit

---
+++ src/main/java/ar/edu/unq/tip/tip_bondi_maps_backend/App.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.tip.tip_bondi_maps_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
