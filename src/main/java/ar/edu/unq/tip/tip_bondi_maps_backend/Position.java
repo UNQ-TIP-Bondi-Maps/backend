e217767f3f9abc336f3294bd0dcc1e83a92d64f1
Luis Zagarella <zagarojo23@gmail.com>
2016-09-13 02:56:45 -0300

    add position class and mockito to pom

---
+++ src/main/java/ar/edu/unq/tip/tip_bondi_maps_backend/Position.java
@@ -0,0 +1,21 @@
+package ar.edu.unq.tip.tip_bondi_maps_backend;
+
+public class Position {
+
+	float lat;
+	float lng;
+
+	public float getLat() {
+		return lat;
+	}
+	public void setLat(float lat) {
+		this.lat = lat;
+	}
+	public float getLng() {
+		return lng;
+	}
+	public void setLng(float lng) {
+		this.lng = lng;
+	}	
+	
+}
