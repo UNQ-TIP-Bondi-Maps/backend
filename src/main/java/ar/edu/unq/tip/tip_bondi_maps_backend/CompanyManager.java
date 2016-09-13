9feccc1ace1509fa5427b09d1c6d817d08c81845
Luis Zagarella <zagarojo23@gmail.com>
2016-09-13 05:17:22 -0300

    Added - CompanyManager

---
+++ src/main/java/ar/edu/unq/tip/tip_bondi_maps_backend/CompanyManager.java
@@ -0,0 +1,49 @@
+package ar.edu.unq.tip.tip_bondi_maps_backend;
+
+public class CompanyManager {
+	
+	private String nameAndSurname;
+	private int phone;
+	private String email;
+	private String userName;
+	private String password;
+	private Company company;
+	
+	public String getNameAndSurname() {
+		return nameAndSurname;
+	}
+	public void setNameAndSurname(String nameAndSurname) {
+		this.nameAndSurname = nameAndSurname;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Company getCompany() {
+		return company;
+	}
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+	
+}
