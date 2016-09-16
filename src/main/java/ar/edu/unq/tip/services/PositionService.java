7e27a4b466329cee0b272bd835c41b4fcb1cf5a9
Luis Zagarella <zagarojo23@gmail.com>
2016-09-15 23:19:49 -0300

    Added - PositionService

---
+++ src/main/java/ar/edu/unq/tip/services/PositionService.java
@@ -0,0 +1,9 @@
+package ar.edu.unq.tip.services;
+
+import ar.edu.unq.tip.model.Position;
+
+public class PositionService extends GenericService<Position> {
+
+	private static final long serialVersionUID = 6149091016226478860L;
+	
+}
