f9efbe7eb8eacd5c7613c4e9e7b94d871a446329
Luis Zagarella <zagarojo23@gmail.com>
2016-09-15 22:08:51 -0300

    Added - BusService

---
+++ src/main/java/ar/edu/unq/tip/services/BusService.java
@@ -0,0 +1,9 @@
+package ar.edu.unq.tip.services;
+
+import ar.edu.unq.tip.model.Bus;
+
+public class BusService extends GenericService<Bus> {
+
+	private static final long serialVersionUID = -6652628622081947349L;
+	
+}
